31544ba5d816561a3f48619757aae516ad32ee81
Rudi Strahl <strahlr@gmail.com>
2021-06-29 20:28:10 -0700

    Modified NoHighlightStyle to be public

--- CareKitUI/CareKitUI/Shared/NoHighlightStyle.swift
+++ CareKitUI/CareKitUI/Shared/NoHighlightStyle.swift
@@ -31,8 +31,8 @@
 import SwiftUI
 
 /// Turns off the highlighted (AKA pressed) state.
-struct NoHighlightStyle: ButtonStyle {
-    func makeBody(configuration: Self.Configuration) -> some View {
+public struct NoHighlightStyle: ButtonStyle {
+    public func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label.contentShape(Rectangle())
     }
 }
